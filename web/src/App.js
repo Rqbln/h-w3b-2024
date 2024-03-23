79d5715408797253ea14baa15f51709fadb27040
Robin :) <118724817+Rqbln@users.noreply.github.com>
2024-03-23 01:41:19 +0100

    Add Quizz à la barre du haut

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import AboutPage from './pages/AboutPage';
+import QuizzPage from "./pages/QuizzPage";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutPage />} />
+                <Route path="/quizz" element={<QuizzPage />} />
             </Routes>
         </Router>
     );
---
+++ web/src/pages/QuizzPage.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Box, CssBaseline, ThemeProvider, createTheme, Typography } from '@mui/material';
+import AppAppBar from '../landing-page/components/AppAppBar';
+import getLPTheme from '../landing-page/getLPTheme';
+import Footer from '../landing-page/components/Footer';
+
+const QuizzPage = () => {
+    const [mode, setMode] = useState('light');
+    const theme = createTheme(getLPTheme(mode));
+
+    const toggleColorMode = () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+            <Box sx={{ pt: 8, pb: 6, textAlign: 'center' }}>
+                <Typography
+                    variant="h2"
+                    component="h1"
+                    sx={{
+                        fontWeight: 'bold',
+                        fontSize: 'clamp(3rem, 10vw, 4rem)',
+                    }}
+                >
+                    Page&nbsp;
+                    <Typography
+                        component="span"
+                        variant="h2"
+                        sx={{
+                            fontSize: 'clamp(3rem, 10vw, 4rem)',
+                            color: (theme) => theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+                        }}
+                    >
+                        Quizz
+                    </Typography>
+                </Typography>
+            </Box>
+            <Footer />
+        </ThemeProvider>
+    );
+};
+
+export default QuizzPage;
