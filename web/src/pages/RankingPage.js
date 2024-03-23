92405749d72312d8c5944617cf49644185e695b6
JohanMB0 <martinborretjohan@gmail.com>
2024-03-23 01:42:17 +0100

    Create RankingPage.js
    rankingpage

---
+++ web/src/pages/RankingPage.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Box, CssBaseline, ThemeProvider, createTheme, PaletteMode } from '@mui/material';
+import AppAppBar from '../landing-page/components/AppAppBar';
+import getLPTheme from '../landing-page/getLPTheme';
+import Footer from '../landing-page/components/Footer';
+import { useState } from "react";
+
+function RankingPage() {
+  const [mode, setMode] = useState('light');
+  const theme = createTheme(getLPTheme(mode));
+
+  const toggleColorMode = () => {
+    setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      <Box sx={{ pt: 8, pb: 6 }}>
+        <h1>Ranking Page</h1>
+      </Box>
+      <Footer />
+    </ThemeProvider>
+  );
+}
+
+export default RankingPage;
