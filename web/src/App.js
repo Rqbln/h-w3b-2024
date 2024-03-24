98de5ae3c957cc25d85255ffb9a2c9bf3ad0c6f0
VTRMAZ <117839232+VTRMAZ@users.noreply.github.com>
2024-03-24 02:13:41 +0100

    comcom

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import AboutPage from './pages/AboutPage';
+import AccountPage from './pages/AccountPage';
 import QuizzPage from "./pages/QuizzPage";
 import RankingPage from "./pages/RankingPage";
 import PrivacyPage from "./pages/PrivacyPage";
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/quizz" element={<QuizzPage />} />
+                <Route path="/account" element={<AccountPage />} />
                 <Route path="/ranking" element={<RankingPage />} />
                 <Route path="/privacy" element={<PrivacyPage />} />
             </Routes>
