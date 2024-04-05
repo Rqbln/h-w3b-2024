a33c0be5d73108815e571ceb6a64e2b05de1557f
Robin :) <118724817+Rqbln@users.noreply.github.com>
2024-04-05 22:16:42 +0200

    Add environnement Hardhat

--- web/test/Lock.js
+++ web/test/Lock.js
@@ -8,19 +8,12 @@ const { ethers } = require("hardhat");
 
 
 describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+    const lockedAmount = 1_000_000_000; // Utilisation directe au lieu de ONE_GWEI
 
-    const lockedAmount = ONE_GWEI;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
-
     const Lock = await ethers.getContractFactory("Lock");
     const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
