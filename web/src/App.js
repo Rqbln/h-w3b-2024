199eb6956d1756802acff3f65fc1767e293f09ca
Rqbln <118724817+Rqbln@users.noreply.github.com>
2024-04-09 11:46:28 +0200

    nettoyage dependances + refonte package.json

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { connection } from './solanaConfig'; // Importez la configuration Solana
-import { sendTransaction } from './solanaActions'; // Importez la fonction d'action Solana
 import LandingPage from './pages/LandingPage';
 import AboutPage from './pages/AboutPage';
 import AccountPage from './pages/AccountPage';
@@ -11,24 +9,6 @@ import PrivacyPage from "./pages/PrivacyPage";
 import './App.css';
 
 function App() {
-    // Exemple de fonction pour envoyer une transaction
-    // Vous devriez intégrer une logique pour obtenir les adresses et les montants réels
-    const handleSendTransaction = async () => {
-        const fromPubkey = 'VotreAdressePublique';
-        const toPubkey = 'AdresseDestination';
-        const lamports = 1000; // Montant à envoyer
-
-        try {
-            await sendTransaction(connection, fromPubkey, toPubkey, lamports);
-            console.log('Transaction envoyée avec succès');
-        } catch (error) {
-            console.error('Erreur lors de l\'envoi de la transaction', error);
-        }
-    };
-
-    // Vous pouvez appeler handleSendTransaction() depuis n'importe où dans votre application
-    // Par exemple, vous pouvez passer cette fonction aux composants qui nécessitent une interaction avec Solana
-
     return (
         <Router>
             <Routes>
--- web/src/index.js
+++ web/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { DevSupport } from "@react-buddy/ide-toolbox";
 import { ComponentPreviews, useInitial } from "./dev";
 import { WalletProvider } from "./components/walletConnection"; // Importez WalletProvider
@@ -21,6 +20,4 @@ root.render(
 );
 
 // Si vous souhaitez commencer à mesurer les performances de votre application, passez une fonction
-// pour enregistrer les résultats (par exemple : reportWebVitals(console.log))
-// ou envoyez-les à un point de terminaison d'analyse. En savoir plus : https://bit.ly/CRA-vitals
-reportWebVitals();
+
